c4097571c74eaf7d617a202d102a661759a8ff7d
Morton <calder.morton-ferguson@yale.edu>
2025-12-26 14:26:49 -0500

    h200 test setup

--- src/run_burau.sh
+++ src/run_burau.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=burau_p5
-#SBATCH --partition=gpu_devel           # Request the GPU partition
+#SBATCH --job-name=burau_h200_test
+#SBATCH --partition=gpu_h200           # Request the GPU partition
 #SBATCH --gres=gpu:1              # Request 1 GPU
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8         # CPU cores for data loading/overhead
-#SBATCH --mem=64G                 # RAM (increase if you hit OOM)
-#SBATCH --time=1:00:00           # Max runtime (hh:mm:ss)
+#SBATCH --cpus-per-task=16         # CPU cores for data loading/overhead
+#SBATCH --mem=32G                 # RAM (increase if you hit OOM)
+#SBATCH --time=0:30:00           # Max runtime (hh:mm:ss)
 #SBATCH --output=slurm_logs/burau_%j.out   # Saves standard output (print statements)
 #SBATCH --error=slurm_logs/burau_%j.err    # Saves errors
 
@@ -28,12 +28,12 @@ export PYTHONUNBUFFERED=1
 # 3. Run the script
 python find_kernel.py \
     --p 5 \
-    --bucket-size 20000 \
-    --chunk-size 40000 \
+    --bucket-size 80000 \
+    --chunk-size 100000 \
     --device cuda \
-    --use-best 40000 \
+    --use-best 80000 \
     --bootstrap-length 5 \
-    --max-length 80 \
-    --checkpoint-dir "checkpoints/run_p5_m80_${SLURM_JOB_ID}"
+    --max-length 140 \
+    --checkpoint-dir "checkpoints/h200_test_${SLURM_JOB_ID}"
 
 module --force purge
