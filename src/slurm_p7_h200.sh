142659ed4355af0d764db3aab1a46c36b558aeb3
Morton <calder.morton-ferguson@yale.edu>
2025-12-27 22:26:08 -0500

    giga sbatch

--- src/slurm_p7_h200.sh
+++ src/slurm_p7_h200.sh
@@ -36,10 +36,10 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 $PYTHON_PATH find_kernel.py \
     --p 7 \
-    --bucket-size 40000 \
-    --chunk-size 40000 \
+    --bucket-size 28000 \
+    --chunk-size 20000 \
     --device cuda \
-    --use-best 40000 \
+    --use-best 50000 \
     --bootstrap-length 5 \
     --max-length 1600 \
     --degree-multiplier 3 \
