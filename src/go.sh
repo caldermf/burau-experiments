6f8c1e23e5f7e87cba2fc4caf70eb3e6a5cb59e1
Morton <calder.morton-ferguson@yale.edu>
2026-01-02 14:01:27 -0800

    haa

--- src/go.sh
+++ src/go.sh
@@ -2,14 +2,14 @@
 # =============================================================================
 # KERNEL ELEMENT SEARCH - H200
 # =============================================================================
-#SBATCH --job-name=v2test
+#SBATCH --job-name=v1test
 #SBATCH --partition=scavenge_gpu
 #SBATCH --gpus=h200:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=64G                 # CRITICAL: Need lots of CPU RAM for checkpointing
-#SBATCH --time=00:03:00            # Scavenge allows longer times
+#SBATCH --time=00:25:00            # Scavenge allows longer times
 #SBATCH --requeue                  # Automatically requeue if preempted
 #SBATCH --signal=B:USR1@60        # Send signal 120s before timeout
 #SBATCH --output=slurm_logs/v2_%j.out
@@ -26,14 +26,13 @@ PYTHON_PATH="/home/com36/.conda/envs/burau_gpu/bin/python"
 export PYTHONUNBUFFERED=1
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-$PYTHON_PATH find_kernel_ultra_v2.py \
+$PYTHON_PATH find_kernel_ultra.py \
     --p 5 \
     --bucket-size 1000000 \
     --device cuda \
     --use-best 500000 \
     --max-length 60 \
-    --matmul-chunk 11000 \
-    --gc-every 4
+    --matmul-chunk 10000
 
 
 echo "JOB COMPLETED!"
