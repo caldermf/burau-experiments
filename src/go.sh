98230a7fb58e7babecf831ce2974f1118e8381c3
Morton <calder.morton-ferguson@yale.edu>
2026-01-03 09:35:59 -0800

    array

--- src/go.sh
+++ src/go.sh
@@ -9,11 +9,12 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=64G                 # CRITICAL: Need lots of CPU RAM for checkpointing
-#SBATCH --time=01:20:00            # Scavenge allows longer times
+#SBATCH --time=00:50:00            # Scavenge allows longer times
 #SBATCH --requeue                  # Automatically requeue if preempted
 #SBATCH --signal=B:USR1@60        # Send signal 120s before timeout
-#SBATCH --output=slurm_logs/jan3test_%j.out
-#SBATCH --error=slurm_logs/jan3test_%j.err
+#SBATCH --array=1-10                 # Defines the range of tasks
+#SBATCH --output=slurm_logs/jan3_%A_%a.out
+#SBATCH --error=slurm_logs/jan3_%A_%a.err
 
 set -e
 mkdir -p slurm_logs checkpoints
