c5bfef1dea8815ccc57bc36340dbb3bf4e7c04a0
Morton <calder.morton-ferguson@yale.edu>
2026-01-03 15:20:38 -0800

    try new

--- src/go.sh
+++ src/go.sh
@@ -7,9 +7,9 @@
 #SBATCH --gpus=h200:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=64G                 # CRITICAL: Need lots of CPU RAM for checkpointing
-#SBATCH --time=00:50:00            # Scavenge allows longer times
+#SBATCH --time=01:30:00            # Scavenge allows longer times
 #SBATCH --requeue                  # Automatically requeue if preempted
 #SBATCH --signal=B:USR1@60        # Send signal 120s before timeout
 #SBATCH --array=1-10                 # Defines the range of tasks
@@ -29,10 +29,10 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 $PYTHON_PATH find_kernel_ultra.py \
     --p 7 \
-    --bucket-size 800000 \
+    --bucket-size 400000 \
     --device cuda \
-    --use-best 400000 \
-    --max-length 127 \
+    --use-best 200000 \
+    --max-length 255 \
     --matmul-chunk 9000
 
 
