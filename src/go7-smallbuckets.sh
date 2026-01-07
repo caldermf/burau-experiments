3e6610563c4e1701b00643329a7eca62ec50a8ec
Morton <calder.morton-ferguson@yale.edu>
2026-01-07 15:34:09 -0800

    Added smaller bucket test script

---
+++ src/go7-smallbuckets.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# =============================================================================
+# KERNEL ELEMENT SEARCH - H200
+# =============================================================================
+#SBATCH --job-name=GO7
+#SBATCH --partition=scavenge_gpu
+#SBATCH --gpus=h200:1
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=64G                 # CRITICAL: Need lots of CPU RAM for checkpointing
+#SBATCH --time=02:30:00            # Scavenge allows longer times
+#SBATCH --requeue                  # Automatically requeue if preempted
+#SBATCH --signal=B:USR1@60        # Send signal 120s before timeout
+#SBATCH --array=1                 # Defines the range of tasks
+#SBATCH --output=slurm_logs/jan7p7_%A_%a.out
+#SBATCH --error=slurm_logs/jan7p7_%A_%a.err
+
+set -e
+mkdir -p slurm_logs checkpoints
+
+module purge
+module load miniconda
+
+PYTHON_PATH="/home/com36/.conda/envs/burau_gpu/bin/python"
+
+export PYTHONUNBUFFERED=1
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+$PYTHON_PATH find_kernel.py \
+    --p 7 \
+    --bucket-size 200_000 \
+    --use-best 1_600_000 \
+    --max-length 127 \
+    --matmul-chunk 8000 \
+    --chunk-size 100_000 \
+    --degree-multiplier 2
+
+
+echo "JOB COMPLETED!"
