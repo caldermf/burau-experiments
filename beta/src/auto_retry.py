b3252594dcb9f48021a4f394df38a0e5ccf73c6c
Calder M-F <calder.morton-ferguson@yale.edu>
2026-01-12 14:23:41 -0500

    Tweaked auto retry

--- beta/src/auto_retry.py
+++ beta/src/auto_retry.py
@@ -31,12 +31,12 @@ def run_auto_search():
     MAX_LENGTH = 63
     
     # The "Give Up" Condition
-    CHECK_LEVEL = 30
-    PROJLEN_CUTOFF = 40  # Must be strictly less than this to continue
+    CHECK_LEVEL = 23
+    PROJLEN_CUTOFF = 21  # Must be strictly less than this to continue
     
     # Search hyperparameters (matched to your log)
-    BUCKET_SIZE = 2000    # Increased slightly from 1000 for robustness
-    USE_BEST = 4000
+    BUCKET_SIZE = 1000    # Increased slightly from 1000 for robustness
+    USE_BEST = 2000
     MATMUL_CHUNK = 8000
     EXPANSION_CHUNK = 50000
     DEGREE_MULTIPLIER = 2
