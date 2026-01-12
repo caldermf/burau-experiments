8cc52f3b9a811330699e4af548bce898be6ac124
Calder M-F <calder.morton-ferguson@yale.edu>
2026-01-11 21:12:20 -0500

    New sbatch trial

--- beta/src/batchy.sh
+++ beta/src/batchy.sh
@@ -7,12 +7,12 @@
 #SBATCH --gpus=1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=6
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=64G                 # CRITICAL: Need lots of CPU RAM for checkpointing
 #SBATCH --time=00:18:00            # Scavenge allows longer times
 #SBATCH --requeue                  # Automatically requeue if preempted
 #SBATCH --signal=B:USR1@60        # Send signal 120s before timeout
-#SBATCH --array=1-12                 # Defines the range of tasks
+#SBATCH --array=13-36                 # Defines the range of tasks
 #SBATCH --output=slurm_logs/moonshot_%A_%a.out
 #SBATCH --error=slurm_logs/moonshot_%A_%a.err
 
@@ -27,6 +27,6 @@ PYTHON_PATH="/home/com36/.conda/envs/burau_gpu/bin/python"
 export PYTHONUNBUFFERED=1
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-$PYTHON_PATH find_kernel.py --n 6 --r 2 --p 3 --bucket-size 10000 --matmul-chunk 8500 --use-best 20000 --bootstrap-length 2 --max-length 127 --chunk-size 70000
+$PYTHON_PATH find_kernel.py --n 6 --r 2 --p 3 --bucket-size 1000 --matmul-chunk 8500 --use-best 2000 --bootstrap-length 2 --max-length 127 --chunk-size 70000
 
 echo "JOB COMPLETED!"
