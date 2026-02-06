3350045d4b596f597c68692c0ac5a772d49ebd1c
Calder M-F <calder.morton-ferguson@yale.edu>
2026-02-06 18:40:15 -0500

    added sbatch for h200

---
+++ triton/go7-127.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# =============================================================================
+# KERNEL ELEMENT SEARCH - H200
+# =============================================================================
+#SBATCH --job-name=MONSTER
+#SBATCH --partition=scavenge_gpu
+#SBATCH --gpus=h200:1
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=64G                 # CRITICAL: Need lots of CPU RAM for checkpointing
+#SBATCH --time=00:15:00            # Scavenge allows longer times
+#SBATCH --requeue                  # Automatically requeue if preempted
+#SBATCH --signal=B:USR1@60        # Send signal 120s before timeout
+#SBATCH --array=1                 # Defines the range of tasks
+#SBATCH --output=slurm_logs/MONSTER_%A_%a.out
+#SBATCH --error=slurm_logs/MONSTER_%A_%a.err
+
+set -e
+mkdir -p slurm_logs checkpoints
+
+module purge
+module load miniconda
+
+PYTHON_PATH="/home/com36/.conda/envs/burau_gpu/bin/python"
+
+export PYTHONUNBUFFERED=1
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+$PYTHON_PATH braid_search.py
+
+
+echo "JOB COMPLETED!"
