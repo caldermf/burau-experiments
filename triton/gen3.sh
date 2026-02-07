5b3ecd10dc0ae00457cdf8ffc33b21e79ae13aca
caldermf <comfyc@gmail.com>
2026-02-07 18:09:43 -0500

    script update

--- triton/gen3.sh
+++ triton/gen3.sh
@@ -25,7 +25,7 @@ PYTHON_PATH="/home/com36/.conda/envs/burau_gpu/bin/python"
 export PYTHONUNBUFFERED=1
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-$PYTHON_PATH braid_search_3.py -u 18 -b 9 -m 33
+$PYTHON_PATH braid_search_3.py -u 12 -b 6 -m 33
 
 
 echo "JOB COMPLETED!"
