42900491e801b734296f92e6a6923502d73480a4
caldermf <comfyc@gmail.com>
2026-02-08 15:25:51 -0500

    batchedmonster script

--- triton/batched_monster.sh
+++ triton/batched_monster.sh
@@ -27,7 +27,7 @@ PYTHON_PATH="/home/com36/.conda/envs/burau_gpu/bin/python"
 export PYTHONUNBUFFERED=1
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-$PYTHON_PATH braid_batched.py --total-use-best 99 --gpu-batch-size 25
+$PYTHON_PATH braid_batched.py --total-use-best 90 --gpu-batch-size 15
 
 
 echo "JOB COMPLETED!"
